45a96c85fe106d14dc00b08403153e5534a3db60
salafee-xs <72187069+salafee-xs@users.noreply.github.com>
2022-10-27 00:38:44 +0600

    Practice Array

--- array/multidimensional-array.php
+++ array/multidimensional-array.php
@@ -11,3 +11,25 @@ var_dump($number);
 echo $number[0][2];
 echo '<br>';
 echo $number[2][4][2];
+echo '<br>';
+
+$student = array(
+    'fname'   => 'Jamal',
+    'lname'   => 'Ahmed',
+    'age'     => '15',
+    'section' => 'B',
+    'Roll'    => 150
+);
+
+echo $student['lname'];
+echo '<br>';
+printf('%s %s', $student['fname'], $student['lname']);
+echo '<br>';
+
+// JSON convert
+$jsondata = json_encode($student);
+echo $jsondata;
+echo '<br>';
+
+$studentarr = json_decode($jsondata, true);
+print_r($studentarr);
