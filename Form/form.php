ea94cbb8976d4ebf51aa84ae087ef901220ece33
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2022-11-11 00:27:59 +0600

    Practice Sanitization.

--- Form/form.php
+++ Form/form.php
@@ -4,22 +4,28 @@
         <div class="col-md-3"></div>
         <div class="col-md-6">
             <h2 class="mb-5">Bootstrap Form</h2>
-            <?php if(isset($_POST['fname']) && !empty($_POST['fname'])) { ?>
-                <p>First Name: <?php echo $_POST['fname']; ?></p>
+            <?php
+                $fname = '';
+                $lname = '';
+
+             if(isset($_REQUEST['fname']) && !empty($_REQUEST['fname'])) { ?>
+                <?php $fname = htmlspecialchars($_REQUEST['fname']); ?>
+                <p>First Name: <?php echo $_REQUEST['fname']; ?></p>
             <?php } ?> 
             
-            <?php if(isset($_POST['fname']) && !empty($_POST['lname'])) { ?>
-                <p>Last Name: <?php echo $_POST['lname']; ?></p>
+            <?php if(isset($_REQUEST['fname']) && !empty($_REQUEST['lname'])) { ?>
+                <?php $lname = htmlspecialchars($_REQUEST['lname']); ?>
+                <p>Last Name: <?php echo $_REQUEST['lname']; ?></p>
             <?php } ?> 
 
-            <form class="align-items-center" method="POST"> <!-- HTTP VERB (GET, POST, PATCH...) -->
+            <form class="align-items-center" method="REQUEST"> <!-- HTTP VERB (GET, POST, PATCH...) -->
                 <div class="col-auto mb-3">
                     <label class="form-check-label" for="fname">First Name</label>
-                    <input type="text" class="form-control" name="fname" id="fname">
+                    <input type="text" class="form-control" name="fname" id="fname" value="<?php echo $fname; ?>">
                 </div>
                 <div class="col-auto mb-3">
                     <label class="form-check-label" for="lname">Last Name</label>
-                    <input type="text" class="form-control" name="lname" id="lname">
+                    <input type="text" class="form-control" name="lname" id="lname" value="<?php echo $lname; ?>">
                 </div>
                 <div class="col-auto">
                     <button type="submit" class="btn btn-primary">Submit</button>
