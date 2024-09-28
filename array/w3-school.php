d0ba6e08c72e51d5b7501a47288c2f3a4404f8f4
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-09-29 00:55:24 +0600

    Multi dimensional Array.

--- array/w3-school.php
+++ array/w3-school.php
@@ -39,4 +39,30 @@ for ($i = 0; $i < count($numbers); $i++) {
 
 echo "<br>";
 echo "<h2>Multidimensional Array.</h2>";
-// Multidimensional array
+
+/**
+ * Multidimensional Array
+ * @param mixed $name, stock, sold
+ * return void
+ */
+
+$car = array(
+    array("BMW", 22, 18),
+    array("Tesla", 30, 25),
+    array("Allion", 40, 34),
+    array("Land Rover", 30, 14),
+);
+
+
+
+echo $car[0][0] . " In stock " . $car[0][1] . " Sold " . $car[0][2] . '<br>';
+$rows = count($car);
+for ($row = 0; $row < $rows; $row++) {
+    echo "<p><b>Row Number</b> $row</p>";
+    echo "<ul>";
+    $cols = count($car[$row]);
+    for ($col = 0; $col < $cols; $col++) {
+        echo "<li>" . $car[$row][$col] . "</li>";
+    }
+    echo "</ul>";
+}
