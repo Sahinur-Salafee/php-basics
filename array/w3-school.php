f7f6bace4f2b57f2d5b33550bd7f2f1c7057d714
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-09-29 00:33:46 +0600

    Array sorting.

--- array/w3-school.php
+++ array/w3-school.php
@@ -20,3 +20,23 @@ $arrlen = count($animal);
 for ($i = 0; $i < $arrlen; $i++) {
     echo $animal[$i] . '<br>';
 }
+echo "<br>";
+
+echo "<h2>Array Sorting.</h2>";
+$cars = array("BMW", "Tesla", "Axio", "Allion");
+$sortcar = sort($cars);
+for ($i = 0; $i < count($cars); $i++) {
+    echo $cars[$i] . '<br>';
+}
+
+$numbers = array(4, 6, 2, 22, 11);
+$sortnum = sort($numbers);
+
+for ($i = 0; $i < count($numbers); $i++) {
+    echo $numbers[$i] . '<br>';
+}
+
+
+echo "<br>";
+echo "<h2>Multidimensional Array.</h2>";
+// Multidimensional array
