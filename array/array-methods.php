f2944d7e657b1a3ce557f2658d870cfc912d6ec5
salafee-xs <47431625+Sahinur-Salafee@users.noreply.github.com>
2024-09-30 06:31:19 +0600

    array_chank method

--- array/array-methods.php
+++ array/array-methods.php
@@ -12,3 +12,15 @@ $age = array(
 print_r(array_change_key_case($age, CASE_UPPER));
 echo '<br>';
 print_r(array_change_key_case($age, CASE_LOWER));
+echo '<br>';
+
+//If two or more keys will be equal after running array_change_key_case() 
+// (e.g. "b" and "B"), the latest array will override the other.
+$pets = array("a" => "Cat", "B" => "Dog", "c" => "Horse", "b" => "Bird");
+print_r(array_change_key_case($pets, CASE_UPPER));
+echo '<br>';
+
+// PHP array_chunk() Function
+
+$cars = array("Volvo", "BMW", "Toyota", "Honda", "Mercedes", "Opel");
+print_r(array_chunk($cars, 3));
